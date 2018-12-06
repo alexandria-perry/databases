ebdf8ee2667c8ec016f3b33c8d24cc334e1770ef
alexandria-perry <aeperry22@gmail.com>
2018-12-05 17:32:59 -0800

    work in progress

--- server/models/index.js
+++ server/models/index.js
@@ -1,15 +1,31 @@
 var db = require('../db');
+var utils = require('./utils');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    get: function (request, response) {
+      utils.sendResponse(response, {results: messages});
+    }, // a function which produces all the messages
+    post: function (request, response) {
+      utils.collectData(request, function(message) {
+            // message.objectId = ++objectIdCounter;
+            // messages.push(message);
+        utils.sendResponse(response, {objectId: message.objectId}, 201);
+      });
+    } // a function which can be used to insert a message into the database
   },
 
   users: {
-    // Ditto as above.
-    get: function () {},
-    post: function () {}
+    get: function (request, response) {
+      utils.sendResponse(response, {results: users});
+    }, // a function which produces all the messages
+    post: function (request, response) {
+      utils.collectData(request, function(message) {
+        user.objectId = ++objectIdCounter;
+        users.push(message);
+        utils.sendResponse(response, {objectId: user.objectId}, 201);
+      });
+    } // a function which can be used to insert a message into the database
   }
 };
 
--- server/schema.sql
+++ server/schema.sql
@@ -2,12 +2,28 @@ CREATE DATABASE chat;
 
 USE chat;
 
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+
+CREATE TABLE rooms (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  id INTEGER PRIMARY KEY,
+  userId INTEGER,
+  message TEXT,
+  roomId INTEGER,
+  FOREIGN KEY (userId)
+    REFERENCES users(id),
+  FOREIGN KEY (roomId)
+    REFERENCES rooms(id)
 );
 
 /* Create other tables and define schemas for them here! */
-
 
 
 
